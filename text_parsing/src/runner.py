491fbaf81ffba219689bcec1e9634e77a2532502
Subrahmanya Joshi <subrahmanyajoshi123@gmail.com>
2021-11-18 23:37:48 +0530

    Added beam pipeline script for text preprocessor

--- text_parsing/src/runner.py
+++ text_parsing/src/runner.py
@@ -3,7 +3,8 @@ from argparse import Namespace
 from datetime import datetime
 
 import apache_beam as beam
-import pandas as pd
+
+from text_parsing.src.preprocessor import Preprocessor
 
 
 class DataFlowSubmitter(object):
@@ -11,8 +12,8 @@ class DataFlowSubmitter(object):
     def __init__(self, args: Namespace):
         self.project = args.project
         self.bucket = args.bucket
-        self.bigquery_table = args.bigquery_table
-        self.output_path = args.output_path
+        self.input_path = args.input_path
+        self.output_dir = args.output_dir
 
         if args.direct_runner and args.dataflow_runner:
             raise ValueError('Please specify only one of the options. either direct runner or dataflow runner')
@@ -22,19 +23,8 @@ class DataFlowSubmitter(object):
             self.runner = 'DataFlowRunner'
 
     @staticmethod
-    def convert_namespace(element):
-        return Namespace(**element)
-
-    @staticmethod
-    def convert_to_df(element):
-        ns_list = element[1]
-        ns_list = [vars(ele) for ele in ns_list]
-        df = pd.DataFrame(ns_list)
-        return df
-
-    @staticmethod
-    def get_info(df):
-        return {'columns': list(df.columns), 'Shape': df.shape}
+    def to_csv(line):
+        return ','.join([str(entry) for entry in line])
 
     def build_and_run(self):
 
@@ -51,15 +41,22 @@ class DataFlowSubmitter(object):
         p = beam.Pipeline(argv=argv)
 
         (p
-         | 'Get data from BigQuery' >> beam.io.ReadFromBigQuery(query=f'Select * From {self.bigquery_table}',
-                                                                use_standard_sql=True,
-                                                                project='text-analysis-323506')
-         | 'Convert to namespace' >> beam.Map(DataFlowSubmitter.convert_namespace)
-         | 'Groupby subject' >> beam.GroupBy('Subject')
-         | 'Convert to dataframe' >> beam.Map(DataFlowSubmitter.convert_to_df)
-         | 'Get dataset info' >> beam.Map(DataFlowSubmitter.get_info)
-         | 'Write info to Cloud Storage' >> beam.io.WriteToText(f"gs://{self.bucket}/output/csv_details-output.txt")
+         | 'Read text file' >> beam.io.ReadFromText(self.input_path)
+         | 'Strip lines' >> beam.Map(Preprocessor.strip_lines)
+         | 'Contract lines' >> beam.Map(Preprocessor.contract_lines)
+         | 'Lower case' >> beam.Map(Preprocessor.to_lower_case)
+         | 'Remove punctuations' >> beam.Map(Preprocessor.remove_punctuations)
+         | 'Remove stopwords' >> beam.Map(Preprocessor.remove_stopwords)
+         | 'Remove special characters' >> beam.Map(Preprocessor.remove_special_chars)
+         | 'Remove emojis' >> beam.Map(Preprocessor.remove_emojis)
+         | 'Split data' >> beam.Map(Preprocessor.split_data)
+         | 'Filter none values' >> beam.Filter(lambda x: x is not None)
+         | 'To csv' >> beam.Map(DataFlowSubmitter.to_csv)
+         | 'Write as csv' >> beam.io.WriteToText(f'{self.output_dir}/train',
+                                                 file_name_suffix='.csv',
+                                                 header='text, label')
          )
+
         p.run()
 
 
@@ -67,8 +64,8 @@ def main():
     parser = argparse.ArgumentParser(description='Running Apache Beam pipelines on Dataflow')
     parser.add_argument('--project', type=str, required=True, help='Project id')
     parser.add_argument('--bucket', type=str, required=True, help='Name of the bucket to host dataflow components')
-    parser.add_argument('--bigquery-table', type=str, required=True, help='id of bigquery table')
-    parser.add_argument('--output-path', type=str, required=True, help='output path to store results')
+    parser.add_argument('--input-path', type=str, required=True, help='path to input data')
+    parser.add_argument('--output-dir', type=str, required=True, help='output dir path to store results')
     parser.add_argument('--direct-runner', type=str, required=False, action='store_true')
     parser.add_argument('--dataflow-runner', type=str, required=False, action='store_true')
     args = parser.parse_args()
